e5b622289e02c1f7a80d9c0def679b76d95cf4d5
Gabo Esquivel <contact@gaboesquivel.com>
2018-11-10 01:01:38 -0600

    working on system setup

--- services/eos-dev/scripts/0000_init-chain.sh
+++ services/eos-dev/scripts/0000_init-chain.sh
@@ -33,13 +33,80 @@ function import_private_key () {
   $cleos wallet import --private-key $1
 }
 
+# Helper funciton to create system accounts
+function create_system_account () {
+  $cleos create account eosio $1 $2
+}
+
+# Creates eosio system accounts
+# https://developers.eos.io/eosio-nodeos/docs/bios-boot-sequence#section-step-3-create-important-system-accounts
+function create_eosio_accounts () {
+  # eosio.bpay
+  BPAY_PVTKEY="5KAVVPzPZnbAx8dHz6UWVPFDVFtU1P5ncUzwHGQFuTxnEbdHJL4"
+  BPAY_PUBKEY="EOS84BLRbGbFahNJEpnnJHYCoW9QPbQEk2iHsHGGS6qcVUq9HhutG"
+  import_private_key $BPAY_PVTKEY
+   eosio.bpay $BPAY_PUBKEY
+  # eosio.msig
+  MSIG_PVTKEY="5JweNBD7uWcysVW9M5GMgvuHCcUhoxopACrK8egMX31WDGQF3qv"
+  MSIG_PUBKEY="EOS7pvbQJxNQMEMBizL6WCCroqQkpYogUNoS3NmETy6XK73dXQsBT"
+  import_private_key $MSIG_PVTKEY
+  create_system_account eosio.msig $MSIG_PUBKEY
+  # eosio.names
+  NAMES_PVTKEY="5J173EL5Wvp9tWW38DpTtbesuE3CpbT8B3C62ub3eFYKLZnVokr"
+  NAMES_PUBKEY="EOS7C9UXs3fZCCxDW8dhLZWtcpoDRW3A9j9qaRj9euLFQDWaGADHJ"
+  import_private_key $NAMES_PVTKEY
+  create_system_account eosio.names $NAMES_PUBKEY
+  # eosio.ram
+  RAM_PVTKEY="5JY6ES262GV87BHufZ6ktu57FGuDThHEEu3xE5kxNZfYJmHqCJo"
+  RAM_PUBKEY="EOS8jj53UWtbdacA8WGpzKN88Y6F6W4AQNHHwR4Upy1F8ewVG2crD"
+  import_private_key $RAM_PVTKEY
+  create_system_account eosio.ram $RAM_PUBKEY
+  # eosio.ramfee
+  RAMFEE_PVTKEY="5JrzFNaKmcnxu5qQupVEqVSaF7SRknRFrjXFo5wLShUL8WJUkL8"
+  RAMFEE_PUBKEY="EOS6u1SNetnKtpi5xzxgcUz9Hn9MC3kUhBeiXMiQzKYuzVcr9j8ft"
+  import_private_key $RAMFEE_PVTKEY
+  create_system_account eosio.ramfee $RAMFEE_PUBKEY
+  # eosio.saving
+  SAVING_PVTKEY="5KeBzreGmgw4FX9zYswvBDMiVQvSTK1UGRuQgj14ZrqbtsdPk7z"
+  SAVING_PUBKEY="EOS7Zt4QsM5bb8PR9TERKDphJ6AxefX8oLngEcc3h5kQtrMQY7b5g"
+  import_private_key $SAVING_PVTKEY
+  create_system_account eosio.saving $SAVING_PUBKEY
+  # eosio.stake
+  STAKE_PVTKEY="5KejNGA7WUFhNVztXa7WBvbWBa7kvcXMWAhtDw9NS6FNG5XU6HT"
+  STAKE_PUBKEY="EOS8TSTK5Tuw3LGyw1KZsWeJGGidUsd5UfKKyUro27xyopu2gE5T1"
+  import_private_key $STAKE_PVTKEY
+  create_system_account eosio.stake $STAKE_PUBKEY
+  # eosio.token
+  TOKEN_PVTKEY="5JnpLZYZoaeWhfda8fT5RjSFQ533EGobUMDbc84uGTJEqYyTsNJ"
+  TOKEN_PUBKEY="EOS6vzfJTSUM51MEWXDyuh2fDDfs5FdRX5hn3teMdroTwaNv6ptAE"
+  import_private_key $STOKEN_PVTKEY
+  create_system_account eosio.token $STOKEN_PUBKEY
+  # eosio.vpay
+  VPAY_PVTKEY="5JtpC9zNRZccTsMSJhNEGru5oqqrXjT2Qz5mBQKDASSLScmjP3i"
+  VPAY_PUBKEY="EOS5A3ZChGL2tL1oJvhN7KScmGUAT4DsxZFEywRShGQHLeN2ndp8W"
+  import_private_key $VPAY_PVTKEY
+  create_system_account eosio.vpay $VPAY_PUBKEY
+}
+
+function deploy_system_contracts () {
+  echo "Deploy eosio.msig"
+  $cleos set contract eosio.msig /contracts/eosio.system
+
+  echo "Deploy bios and create token..."
+  $cleos set contract eosio /contracts/eosio.bios
+  $cleos create account eosio eosio.token $TOKEN_PUBKEY $TOKEN_PUBKEY
+  $cleos set contract eosio.token /contracts/eosio.token -p eosio.token
+  $cleos push action eosio.token create '[ "eosio", "1000000000.0000 EOS", 0, 0, 0]' -p eosio.token
+  sleep .5
+
+  echo "Deploy eosio.system"
+  $cleos set contract eosio /contracts/eosio.system
+}
+
 # initialize chain
 function initialize () {
-  # EOSIO keys
-  EOSIO_PVTKEY="5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
-  EOSIO_PUBKEY="EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV"
 
-  # EOSLOCAL keys
+  # eoslocal
   EOSLOCAL_OWNER_PVTKEY="5KacG2v3XYrjmxazgriHVo1updD7PKXJMWzcaQmBMMXE9Y69aW9"
   EOSLOCAL_OWNER_PUBKEY="EOS88bvtAMTwPBQyF8cxFUFXez9zCoebABS3dXngdNphqNtiszLQh"
 
@@ -47,20 +114,10 @@ function initialize () {
   EOSLOCAL_ACTIVE_PUBKEY="EOS8G66UbcXKfQ7unJES7BrKHggQMZfHUkTMkMF8nEbsktpjsb9tr"
 
   echo "Importing eosio and eoslocal keys"
-  import_private_key $EOSIO_PVTKEY
   import_private_key $EOSLOCAL_OWNER_PVTKEY
   import_private_key $EOSLOCAL_ACTIVE_PVTKEY
-  sleep .5
 
-  echo "Deploy bios and token..."
-  $cleos set contract eosio /contracts/eosio.bios -p eosio
-  $cleos create account eosio eosio.token $EOSIO_PUBKEY $EOSIO_PUBKEY
-  $cleos set contract eosio.token /contracts/eosio.token -p eosio.token
-  $cleos push action eosio.token create '[ "eosio", "1000000000.0000 EOS", 0, 0, 0]' -p eosio.token
-  sleep .5
 
-  echo "Deploy eosio.system"
-  $cleos set contract eosio /contracts/eosio.system -p eosio
 
   echo "Creates eoslocal account with stake..."
   $cleos system newaccount --stake-net '10 EOS' --stake-cpu '10 EOS' --buy-ram '10 EOS' eosio eoslocal $EOSLOCAL_OWNER_PUBKEY $EOSLOCAL_ACTIVE_PUBKEY
